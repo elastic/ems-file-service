909de1d39ab9bb65b61e7ae95cf61acd9068129f
Nick Peihl <nick.peihl@elastic.co>
2018-03-08 17:10:38 -0800

    Allow Hjson sources and map source fields to EMS fields

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-Elastic Map Service Data Sources
+# Elastic Map Service Data Sources
 
-Data sources for use in Elastic Map Service
+Machine readable and standardized data sources for use in Elastic Map Service.
 
 ## Usage
 
-Create a new json file in the appropriate folder in `sources`. The json file must match the schema in `schema/source_schema.json`.
+Create a new JSON or [Hjson](http://hjson.org) file in the appropriate folder in `sources`. The source file must match the schema in `schema/source_schema.json`.
 
 To validate data sources against the schema run
 ```node
---
+++ test.js
@@ -0,0 +1,26 @@
+const tape = require('tape')
+const Ajv = require('ajv')
+const Hjson = require('hjson')
+const schema = require('./schema/source_schema.json')
+const glob = require('glob')
+const fs = require('fs')
+
+const ajv = new Ajv()
+ajv.addMetaSchema(require('ajv/lib/refs/json-schema-draft-04.json'), 'http://json-schema.org/draft-04/schema#')
+testAllSources(ajv.compile(schema))
+
+function testAllSources (validate) {
+    glob.sync('sources/**/*.*json').forEach((source) => {
+        tape(`tests for ${source}`, (t) => {
+            try {
+                const data = Hjson.parse(fs.readFileSync(source, 'utf8'))
+                const valid = validate(data)
+
+                t.ok(valid, `${source}: ${JSON.stringify(validate.errors)}`)
+            } catch (err) {
+                t.fail(`could not parse ${source} as JSON ${err}`)
+            }
+            t.end()
+        })
+    })
+}
