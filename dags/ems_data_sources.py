292b37ac712cfabb0fc3fa00f9de96cc485f45bc
Nick Peihl <nick.peihl@elastic.co>
2018-05-21 18:12:18 -0700

    Pull task functions in separate module

--- dags/ems_data_sources.py
+++ dags/ems_data_sources.py
@@ -4,23 +4,19 @@ from airflow.operators.email_operator import EmailOperator
 from airflow.models import DAG
 
 from datetime import datetime, timedelta
-import geopandas as gpd
-import pandas as pd
-import numpy as np
-import jsondiff
-import tempfile
-import requests
-import urllib
 import urllib.parse as urlparse
+import tempfile
 import hjson
-import json
 import os
+from utils.helpers import fc_to_gdf, diff_gdf_geom, diff_gdf_props, retrieve_data
+
+EMS_URL = 'http://vector.maps.elastic.co'
 
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2018,5,20),
+    'start_date': datetime(2018, 5, 20),
     'email': ['nick.peihl@elastic.co'],
     'email_on_failure': True,
     'email_on_retry': False,
@@ -29,8 +25,6 @@ default_args = {
     'catchup': False
 }
 
-EMS_URL = 'http://vector.maps.elastic.co'
-
 SOURCE_FILES = [
     'ca/provinces.hjson',
     'de/states.hjson',
@@ -38,72 +32,30 @@ SOURCE_FILES = [
     'zh/provinces.hjson'
 ]
 
-def retrieve_data (url):
-    fullUrl = urlparse.urljoin(EMS_URL, url)
-    r = requests.get(fullUrl)
-    r.raise_for_status()
-    return r.text
 
-def get_fieldMap_fields (fieldMap):
-    return fieldMap['dest']
-
-def test_regression (fields, **kwargs):
-    existing_data = kwargs['task_instance'].xcom_pull(task_ids='retrieve_existing_data')
-    test_data = kwargs['task_instance'].xcom_pull(task_ids='extract')
-    print(test_data)
-    return False
-
-def fc_to_gdf (fc):
-    """
-    Create a Geopandas GeoDataFrame from a GeoJSON feature collection 
-    stored in a Python dict. 
-    We create a temporary file because `GeoDataFrame.from_features`
-    drops the `id` from the Features. See 
-    https://github.com/geopandas/geopandas/issues/225#issuecomment-324301286
-    """
-    with tempfile.NamedTemporaryFile() as f:
-        f.write(fc.encode('utf-8'))
-        f.flush()
-        os.fsync(f)
-        gdf = gpd.read_file(f.name)
-    gdf.set_index('id', inplace=True)
-    return gdf
-
-def diff_gdf_props (gdf1, gdf2):
-    df1 = pd.DataFrame(gdf1[[col for col in gdf1.columns if col != gdf1._geometry_column_name]])
-    df2 = pd.DataFrame(gdf2[[col for col in gdf2.columns if col != gdf2._geometry_column_name]])
-    json1 = json.loads(df1.to_json(orient='index', force_ascii=False))
-    json2 = json.loads(df2.to_json(orient='index', force_ascii=False))
-    diff = jsondiff.diff(json1, json2, syntax='explicit')
-    return diff
-
-def diff_gdf_geom (gdf1, gdf2):
-    gs1 = gdf1[gdf1._geometry_column_name].buffer(0) # buffer 0 fixes invalid geometry
-    gs2 = gdf2[gdf2._geometry_column_name].buffer(0)
-    diff = gs1.geom_almost_equals(gs2)
-    changed_ids = diff[~diff].index.tolist()
-    return changed_ids
-
-def compare_ds (**kwargs):
-    (sourceDs, existingDs) = kwargs['task_instance'].xcom_pull(key=None, task_ids=['extract_source_data','retrieve_existing_data'])
+def compare_ds(**kwargs):
+    (sourceDs, existingDs) = kwargs['task_instance'].xcom_pull(
+        key=None, task_ids=['extract_source_data', 'retrieve_existing_data'])
     sourceDf = fc_to_gdf(sourceDs)
     existingDf = fc_to_gdf(existingDs)
-    if (existingDf.equals(sourceDf)):
-        return 'cleanup'
-    else:
-        prop_diff = diff_gdf_props(existingDf, sourceDf)
-        geom_diff = diff_gdf_geom(existingDf, sourceDf)
-        kwargs['task_instance'].xcom_push(key='properties_changes', value=prop_diff)
-        kwargs['task_instance'].xcom_push(key='geometry_changes', value=geom_diff)
+    prop_diff = diff_gdf_props(existingDf, sourceDf)
+    geom_diff = diff_gdf_geom(existingDf, sourceDf)
+    if prop_diff or geom_diff:
+        kwargs['task_instance'].xcom_push(
+            key='properties_changes', value=prop_diff)
+        kwargs['task_instance'].xcom_push(
+            key='geometry_changes', value=geom_diff)
         return 'data_changed'
+    else:
+        return 'cleanup'
 
-def create_dag (source_file):
+
+def create_dag(source_file):
     with open(source_file, 'r') as f:
         source = hjson.load(f, object_pairs_hook=dict)
         dag_id = 'dag_{}'.format(source['name'])
         qs = urlparse.urlencode(source['query'])
         fullUrl = urlparse.urljoin(source['data'], '?{}'.format(qs))
-        tmpFile = tempfile.mkstemp(suffix='.json', prefix=dag_id)[1]
         dag = DAG(
             dag_id,
             default_args=default_args,
@@ -146,8 +98,8 @@ def create_dag (source_file):
 
             Feature Ids with geometry changes: 
                 <ul>
-                {% for id in ti.xcom_pull(task_ids='compare_datasets', key='geometry_changes')  %}
-                    <li>{{ id }}</li>
+                {% for f in ti.xcom_pull(task_ids='compare_datasets', key='geometry_changes')  %}
+                    <li>{{ f }}</li>
                 {% endfor %}
                 </ul>
             </div>
@@ -156,10 +108,10 @@ def create_dag (source_file):
 
         data_change = EmailOperator(
             task_id='data_changed',
-            to = 'nick.peihl@elastic.co',
-            subject = 'A source dataset for EMS has changed',
-            html_content = HTML_CONTENT_TEMPLATE,
-            params = {
+            to='nick.peihl@elastic.co',
+            subject='A source dataset for EMS has changed',
+            html_content=HTML_CONTENT_TEMPLATE,
+            params={
                 'name': source['humanReadableName']
             },
             dag=dag
@@ -170,9 +122,9 @@ def create_dag (source_file):
         compare_datasets.set_downstream([data_change, cleanup])
         return dag
 
+
 for f in SOURCE_FILES:
     abs_file = os.path.join('/sources', f)
     dag = create_dag(abs_file)
     dag_id = dag.dag_id
     globals()[dag_id] = dag
-    
