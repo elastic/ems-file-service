f86a4a7f229e5adc2b15de9c152e7efb3d4261a1
Nick Peihl <nick.peihl@elastic.co>
2018-08-14 15:10:16 -0700

    Apply Elastic License

--- scripts/generate-manifest.js
+++ scripts/generate-manifest.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
 const semver = require('semver');
 const _ = require('lodash');
 const constants = require('./constants');
