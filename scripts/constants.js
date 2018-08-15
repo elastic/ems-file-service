32b2ad1d8a5bf9574623cd14e4f37dc5d591514b
Nick Peihl <nickpeihl@gmail.com>
2018-08-15 10:02:03 -0700

    Apply Elastic License (#22)
    Apply Elastic License to source code and maintain Open Database License (ODbL) for vector data in the `data` folder.

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
 module.exports = Object.freeze({
   PRODUCTION_HOST: 'vector.maps.elastic.co',
   STAGING_HOST: 'staging-dot-elastic-layer.appspot.com',
