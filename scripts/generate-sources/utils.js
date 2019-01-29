f8abc406b1ad6b2e47015c40a7c16c5fba4610d6
Nick Peihl <nick.peihl@elastic.co>
2019-01-29 09:27:52 -0800

    Add README and license

--- scripts/generate-sources/utils.js
+++ scripts/generate-sources/utils.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
 function titleCase(str) {
   return str.replace(/\w\S*/g, (str) => `${str.charAt(0).toUpperCase()}${str.substr(1).toLowerCase()}`);
 }
