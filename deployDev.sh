1b8d27c56574582c5d168f3964c5e4b1942229c2
Nick Peihl <nick.peihl@elastic.co>
2019-07-02 16:32:56 -0700

    Add ci job for feature-layers branch

---
+++ deployDev.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#
+# Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+# or more contributor license agreements. Licensed under the Elastic License;
+# you may not use this file except in compliance with the Elastic License.
+#
+
+set -e
+set +x
+
+# Expected env variables:
+# * GPROJECT - e.g. "elastic-ems"
+# * GCE_ACCOUNT - credentials for the google service account (JSON blob)
+
+if [[ -z "${GPROJECT}" ]]; then
+    echo "GPROJECT is not set, e.g. 'GPROJECT=elastic-bekitzur'"
+    exit 1
+fi
+
+export EMS_PROJECT="feature-layers"
+
+export TILE_HOST="tiles.maps.elastic.co"
+export VECTOR_HOST="storage.googleapis.com/${GPROJECT}-${EMS_PROJECT}-vector"
+export CATALOGUE_BUCKET=${GPROJECT}-${EMS_PROJECT}-catalogue
+export VECTOR_BUCKET=${GPROJECT}-${EMS_PROJECT}-vector
+
+unset ARCHIVE_BUCKET
+
+./build.sh
