795b5a43a4a94863b9c6b2404911806a459034d1
Nick Peihl <nick.peihl@elastic.co>
2019-07-03 10:20:25 -0700

    Review feedback

--- deployDev.sh
+++ deployDev.sh
@@ -24,6 +24,8 @@ export TILE_HOST="tiles.maps.elastic.co"
 export VECTOR_HOST="storage.googleapis.com/${GPROJECT}-${EMS_PROJECT}-vector-dev"
 export CATALOGUE_BUCKET=${GPROJECT}-${EMS_PROJECT}-catalogue-dev
 export VECTOR_BUCKET=${GPROJECT}-${EMS_PROJECT}-vector-dev
+export VECTOR_HOST="storage.googleapis.com/${VECTOR_BUCKET}"
+
 
 unset ARCHIVE_BUCKET
 
