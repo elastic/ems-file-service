30e7235d2aec706b299f0ac29dc1e0ddb685ce20
Jorge Sanz <jorge.sanz@elastic.co>
2020-10-06 10:03:24 +0200

    World Countries update (#188)
    Co-authored-by: Nick Peihl <nickpeihl@gmail.com>

---
+++ scripts/duplicate-regions.js
@@ -0,0 +1,74 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License;
+ * you may not use this file except in compliance with the Elastic License.
+ */
+
+const fs = require('fs');
+const csvParse = require('csv-parse/lib/sync');
+const argv = require('yargs')
+  .usage('Script to extract regions overriding region and country data from a CSV')
+  .version('0.1')
+  .epilog('Elastic, 2020')
+  .example('$0 duplicated-regions.csv in.geojson out.json')
+  .help()
+  .alias('h', 'help')
+  .option('verbose', {
+    alias: 'v',
+    default: false,
+    type: 'boolean',
+    describe: 'Log about the process',
+  })
+  .demandCommand(3)
+  .argv;
+
+const [idsPath, inPath, outPath] = argv._;
+
+function log(message) {
+  if (argv.verbose) {
+    console.log(message);
+  }
+}
+
+if (!idsPath || !inPath || !outPath) {
+  throw new Error(`Please provide all parameters`);
+} else {
+  log(`Processing ${inPath} to ${outPath} using ${idsPath}`);
+}
+
+// get the data objects
+const ids = csvParse(fs.readFileSync(idsPath), {
+  columns: true,
+  skip_empty_lines: true,
+});
+
+const input = JSON.parse(fs.readFileSync(inPath));
+
+const inputFeatures = input.features;
+const outputFeatures = [];
+// eslint-disable-next-line camelcase
+for (const { in_region_iso, out_region_name, out_region_iso2, out_country_name, out_country_iso2, out_country_iso3 } of ids) {
+  const regions = inputFeatures
+    // eslint-disable-next-line camelcase
+    .filter(f => f.properties.iso_3166_2 === in_region_iso)
+    .map(f => {
+      return {
+        'type': 'Feature',
+        'geometry': f.geometry,
+        'properties': {
+          name: out_region_name,
+          iso_3166_2: out_region_iso2,
+          iso_a2: out_country_iso2,
+          adm0_a3: out_country_iso3,
+          admin: out_country_name,
+        },
+      };
+    });
+  outputFeatures.push(...regions);
+}
+
+log(`Output dataset with ${outputFeatures.length} fatures`);
+
+const output = { 'type': 'FeatureCollection', 'features': outputFeatures };
+
+fs.writeFileSync(outPath, JSON.stringify(output));
