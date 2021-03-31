349bf43a4222c9402e859615a878caff2fbcedbb
Nick Peihl <nick.peihl@elastic.co>
2021-03-31 15:40:30 -0700

    Add future 7.x releases (#207)

--- scripts/constants.js
+++ scripts/constants.js
@@ -5,9 +5,26 @@
  */
 
 module.exports = Object.freeze({
-  VECTOR_PRODUCTION_HOST: 'vector.maps.elastic.co',
-  VECTOR_STAGING_HOST: 'vector-staging.maps.elastic.co',
-  TILE_PRODUCTION_HOST: 'tiles.maps.elastic.co',
-  TILE_STAGING_HOST: 'tiles.maps.elstc.co',
-  VERSIONS: ['v1', 'v2', 'v6.6', 'v7.0', 'v7.2', 'v7.6', 'v7.7', 'v7.8', 'v7.9', 'v7.10','v7.11', 'v7.12'],
+  VECTOR_PRODUCTION_HOST: "vector.maps.elastic.co",
+  VECTOR_STAGING_HOST: "vector-staging.maps.elastic.co",
+  TILE_PRODUCTION_HOST: "tiles.maps.elastic.co",
+  TILE_STAGING_HOST: "tiles.maps.elstc.co",
+  VERSIONS: [
+    "v1",
+    "v2",
+    "v6.6",
+    "v7.0",
+    "v7.2",
+    "v7.6",
+    "v7.7",
+    "v7.8",
+    "v7.9",
+    "v7.10",
+    "v7.11",
+    "v7.12",
+    "v7.13",
+    "v7.14",
+    "v7.15",
+    "v7.16",
+  ],
 });
