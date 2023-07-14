af3690333b4c2177b9b2986b6b959eff75e4a539
Jorge Sanz <jorge.sanz@elastic.co>
2023-07-14 16:22:48 +0200

    Using getUTC* methods and updating tests

--- scripts/date-versions.js
+++ scripts/date-versions.js
@@ -28,9 +28,9 @@ function formatDateToIso(date) {
     );
   }
 
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
+  const year = date.getUTCFullYear();
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+  const day = String(date.getUTCDate()).padStart(2, "0");
 
   return `${year}-${month}-${day}`;
 }
--- test/manifest-date.js
+++ test/manifest-date.js
@@ -25,16 +25,23 @@ const constants = require("../scripts/constants");
 tap("formatDateToIso", (t) => {
   let validDate = "2023-12-31";
   t.same(
-    validDate,
     formatDateToIso(new Date(validDate)),
+    validDate,
     "Render a valid ISO date"
   );
 
-  let invalidDate = "2023-07-13T14:05:53+0200";
+  let invalidDate = "2023-12-31T23:45:00";
   t.same(
-    "2023-07-13",
     formatDateToIso(new Date(invalidDate)),
-    "Reduce a full timestamp into a date only string"
+    "2023-12-31",
+    "Reduce a timestamp without time zone into a date only string"
+  );
+
+  invalidDate = "2023-12-31T23:45:00-1200";
+  t.same(
+    formatDateToIso(new Date(invalidDate)),
+    "2024-01-01",
+    "Reduce a full timestamp with time zone into a date only string"
   );
 
   t.throws(
