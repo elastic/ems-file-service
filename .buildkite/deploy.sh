6c617384e1b813b5069986de19c625e3397b3c70
Jorge Sanz <jorge.sanz@elastic.co>
2024-02-15 16:57:05 +0100

    Print a simple file with commit hash in the distribution root

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -63,7 +63,8 @@ if [[ -z "${CATALOGUE_BUCKET}" || -z "${VECTOR_BUCKET}" ]]; then
     exit 1
 fi
 
-export GCE_ACCOUNT_SECRET=$(retry 5 vault read --field=value ${GCS_VAULT_SECRET_PATH})
+export GCE_ACCOUNT_SECRET
+GCE_ACCOUNT_SECRET=$(retry 5 vault read --field=value "${GCS_VAULT_SECRET_PATH}")
 unset GCS_VAULT_SECRET_PATH
 
 if [[ -z "${GCE_ACCOUNT_SECRET}" ]]; then
@@ -82,11 +83,12 @@ unset GCE_ACCOUNT_SECRET
 echo "--- :yarn: Build the assets"
 yarn install
 yarn build
+printf "EMS File Service\n%.8s\n" "$(git rev-parse HEAD)" > ./dist/index.html
 
 
 # Archive the assets if ARCHIVE_BUCKET is set
 if [[ -n "${ARCHIVE_BUCKET}" ]]; then
-    TIMESTAMP=`date +"%Y-%m-%d_%H-%M-%S"`
+    TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
     SNAPSHOT_DIR="./${TIMESTAMP}_snapshot"
     ZIP_FILE=${TIMESTAMP}_emsfiles.tar.gz
     ZIP_FILE_PATH=./$ZIP_FILE
